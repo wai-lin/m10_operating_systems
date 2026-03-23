0af9cdcfe9296a3001766ad217085b66d0b32af7
wai-lin <dev.wailin@gmail.com>
2026-03-23 21:25:15 +0700

    feat: schroedinger solved

--- schroedinger_cats/cat.c
+++ schroedinger_cats/cat.c
@@ -1,22 +1,24 @@
 #define _GNU_SOURCE // needed for execvp
-#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <unistd.h>
 
 void main() {
-     // initialize random number generator from system timer with nanosecond precision
-    struct timespec current_time;
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &current_time);
-    srand(current_time.tv_nsec);
+  // initialize random number generator from system timer with nanosecond
+  // precision
+  struct timespec current_time;
+  clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &current_time);
+  srand(current_time.tv_nsec);
 
-    int alive = rand() % 2;
+  int alive = rand() % 2;
 
-    if (alive) {
-        printf("alive! =^o.o^=\n");
-        for (;;) sleep(1);
-    } else {
-        printf("dead   =^x.x^=\n");
-        _exit(EXIT_FAILURE);
-    }
+  if (alive) {
+    printf("alive! =^o.o^=\n");
+    for (;;)
+      sleep(1);
+  } else {
+    printf("dead   =^x.x^=\n");
+    _exit(EXIT_FAILURE);
+  }
 }
--- schroedinger_cats/schroedinger.c
+++ schroedinger_cats/schroedinger.c
@@ -1,6 +1,6 @@
+#include <spawn.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <spawn.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -9,24 +9,33 @@ extern char **environ;
 
 int main() {
 
-    const int NUM_CATS = 100;
-    pid_t child_pids[100];
+  const int NUM_CATS = 100;
+  pid_t child_pids[100];
 
-    for (int i = 0; i < NUM_CATS; i++) {
-        // Argv are command line arguments.
-        // By convention, the first arguments needs to be the command name.
-        char* argv[] = {"cat", NULL};
-        pid_t last_child_pid;
-        // The first argument to posix_spawn is a pointer - figure out why.
-        int status = posix_spawn(&last_child_pid, "./cat", NULL, NULL, argv, NULL);
-        if (status < 0) {
-            printf("failed to spawn a cat, terminating the experiment\n");
-            return 1;
-        }
+  for (int i = 0; i < NUM_CATS; i++) {
+    // Argv are command line arguments.
+    // By convention, the first arguments needs to be the command name.
+    char *argv[] = {"cat", NULL};
+    pid_t last_child_pid;
+    // The first argument to posix_spawn is a pointer - figure out why.
+    // Ans: We have to provide pointer to store the process id.
+    int status = posix_spawn(&last_child_pid, "./cat", NULL, NULL, argv, NULL);
+    child_pids[i] = last_child_pid;
+    if (status != 0) {
+      printf("failed to spawn a cat, terminating the experiment\n");
+      return 1;
     }
-    sleep(1);
+  }
+  sleep(1);
 
-    // Now, measure the number of cats still alive!
-    int cats_alive; // = ???
-    printf("%d cats are ok\n", cats_alive);
+  // Now, measure the number of cats still alive!
+  int cats_alive = 0; // = ???
+  for (int i = 0; i < NUM_CATS; i++) {
+    int status;
+    pid_t result = waitpid(child_pids[i], &status, WNOHANG);
+    if (result == 0) {
+      cats_alive++;
+    }
+  }
+  printf("%d cats are ok\n", cats_alive);
 }
